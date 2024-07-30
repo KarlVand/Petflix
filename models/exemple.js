c701aa09d3d4a01e6fe3f234b014836b439cc537
Adam <delsarte.adam@gmail.com>
2024-07-30 09:07:41 +0200

    bug fix table creation

--- models/exemple.js
+++ models/exemple.js
@@ -6,7 +6,6 @@ module.exports = (sequelize) => {
     {
       title: Sequelize.STRING,
       author: Sequelize.STRING,
-      body: Sequelize.TEXT,
     },
     { sequelize }
   );
@@ -16,3 +15,6 @@ module.exports = (sequelize) => {
 
 // DATE data type accepts a date value provided in yyyy-mm-dd hh:mm:ss format,
 // while DATEONLY accepts a date value in yyyy-mm-dd format (DATE without time)
+
+// ajouter const Article = require('../models').Article;  dans le fichier de travail
+// const article = await Article.create(req.body); ext .....
--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
+const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
@@ -23,11 +24,17 @@ if (config.use_env_variable) {
 fs.readdirSync(__dirname)
   .filter((file) => {
     return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
     );
   })
   .forEach((file) => {
-    const model = require(path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
