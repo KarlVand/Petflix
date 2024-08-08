d1db937c8a510a904765b86bced8619f979dc465
Juan <j.imbaquingo@outlook.fr>
2024-08-08 12:25:03 +0200

    last

--- public/javascripts/server.js
+++ public/javascripts/server.js
@@ -3,16 +3,28 @@ const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 const app = express();
 const port = 3000;
+const pathObj = require("path");
+const url = require("url");
+const { fileURLToPath } = require("url");
+const meta = require("url");
+const selectedFile = fileURLToPath(import.meta.url);
+const directoryPath = pathObj.dirname(selectedFile);
 
-// Se connecter a la db
+// Conectar a la base de datos db1
 const db = new sqlite3.Database("./movies.db");
-const db2 = new sqlite3.Database("./movies2.db");
+// Conectar a la base de datos db2
+const db2 = new sqlite3.Database("./db2.db");
 
+// Servir archivos CSS y JS desde el directorio 'public'
+app.use("/stylesheets", express.static(path.join(__dirname, "../stylesheets")));
+app.use("/javascript", express.static(path.join(__dirname, "../javascript")));
 
-// Servir les fichiers statiques comme : HTML, CSS, JS
-app.use(express.static(path.join(__dirname, "public")));
+// Servir el archivo HTML desde el directorio 'html'
+app.get("/", (req, res) => {
+  res.sendFile("frontEnd.html", { root: directoryPath });
+});
 
-// Configuration du endpoint pour obtenir les films
+// Endpoint para obtener películas de db1
 app.get("/api/movies", (req, res) => {
   db.all("SELECT * FROM movies", [], (err, rows) => {
     if (err) {
@@ -23,6 +35,18 @@ app.get("/api/movies", (req, res) => {
   });
 });
 
+// Endpoint para obtener películas de db2
+app.get("/api/movies2", (req, res) => {
+  db2.all("SELECT * FROM movies", [], (err, rows) => {
+    if (err) {
+      res.status(500).send(err.message);
+      ("");
+    }
+    res.json(rows);
+  });
+});
+
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
